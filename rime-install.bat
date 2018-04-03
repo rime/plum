175e33e479a0c7db4733a01b7f5e41a1efa82cca
Chen Gong <chen.sst@gmail.com>
2018-04-03 19:05:49 +0800

    feat(rime-install.bat): try downloading git with curl, powershell, or print manual instructions

--- rime-install.bat
+++ rime-install.bat
@@ -19,21 +19,47 @@ if "%git_mirror%" == "taobao" (
 set git_download_url=%git_download_url_prefix%v%git_version%.windows%git_release%/%git_installer%
 
 where /q bash
-if %errorlevel% neq 0 (
-   if exist %git_installer% (
-      echo Found installer: %git_installer%
-   ) else (
-      echo Downloading installer: %git_installer%
-      curl -fsSLO %git_download_url%
-      if not exist %git_installer% (
-         echo Error downloading %git_installer%
-         exit /b 1
-      )
-      echo Download complete: %git_installer%
-   )
-   echo Installing git ...
-   %git_installer% /GitAndUnixToolsOnPath
+if %errorlevel% equ 0 goto :bash_found
+
+if exist %git_installer% (
+   echo Found installer: %git_installer%
+   rem TODO: verify installer
+   goto :install_git
 )
+
+where /q curl
+if %errorlevel% equ 0 (
+   set downloader=curl -fsSL
+   set save_to=-o
+   goto :download_git
+)
+
+where /q powershell
+if %errorlevel% equ 0 (
+   set downloader=powershell Invoke-WebRequest
+   set save_to=-OutFile
+   goto :download_git
+)
+
+echo Error: neither curl nor powershell is found.
+echo Please manually download and install git, then re-run %~n0:
+echo %git_download_url%
+exit /b 1
+
+:download_git
+echo Downloading installer: %git_installer%
+%downloader% %git_download_url% %save_to% %git_installer%
+if not exist %git_installer% (
+echo Error downloading %git_installer%
+exit /b 1
+)
+echo Download complete: %git_installer%
+
+:install_git
+echo Installing git ...
+%git_installer% /GitAndUnixToolsOnPath
+
+:bash_found
 
 set PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\Git\mingw%arch%\bin;%ProgramFiles%\Git\usr\bin;%PATH%
 rem path
