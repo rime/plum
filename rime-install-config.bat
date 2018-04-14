df75dfd5b3bc520d7db659c0aefef9e5ed0df3af
Chen Gong <chen.sst@gmail.com>
2018-04-14 20:14:42 +0800

    chore(rime-install-config.bat): template for customizing the package installer

---
+++ rime-install-config.bat
@@ -0,0 +1,18 @@
+rem Location of download cache
+rem set download_cache_dir=%TEMP%
+
+rem China mirror for downloading Git for Windows installer
+rem set git_mirror=taobao
+
+rem Do not update packages; only download missing files.
+rem CAUTION: may suffer from incomplete downloads.
+rem set no_update=1
+
+rem Location of Rime configuration manager and downloaded packages
+rem set plum_dir=%APPDATA%\plum
+
+rem Location of Rime user directory
+rem set rime_dir=%APPDATA%\Rime
+
+rem Disable /plum/ bash script; use batch installer only.
+rem set use_plum=0
