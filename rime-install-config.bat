b43d23ae1a104591ecca5693e682985ba17e6b71
fxliang <fx.liang@outlook.com>
2024-02-18 13:26:48 +0800

    fix: rime user directory not correct if it's customized

--- rime-install-config.bat
+++ rime-install-config.bat
@@ -14,5 +14,9 @@ rem set plum_dir=%APPDATA%\plum
 rem Location of Rime user directory
 rem set rime_dir=%APPDATA%\Rime
 
+set key=HKEY_CURRENT_USER\SOFTWARE\Rime\Weasel
+set name=RimeUserDir
+for /f "tokens=2*" %%a in ('reg query "%key%" /v "%name%"') do set rime_dir=%%b
+
 rem Disable /plum/ bash script; use batch installer only.
 rem set use_plum=0
